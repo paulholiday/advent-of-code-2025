31038036a1bd244180a89f39c119336da1f1972a
paulholiday <paul.holiday@ensono.com>
2026-01-07 18:35:38 +0000

    Day One part two

--- src/main/java/org/paulholiday/SafeDoor.java
+++ src/main/java/org/paulholiday/SafeDoor.java
@@ -4,21 +4,23 @@ package org.paulholiday;
 
 public class SafeDoor {
 
-
-    public int turnDial(int currentDialPosition, String dialMovement) {
+    public DialContext turnDial(DialContext currentDialContext, String dialMovement) {
         DialDirection direction = DialDirection.RIGHT;
         if (dialMovement.startsWith("L")) {
             direction = DialDirection.LEFT;
         }
 
         int numberOfClicks = Integer.parseInt(dialMovement.substring(1));
-        int newDialPosition = currentDialPosition;
+        int newDialPosition = currentDialContext.getCurrentDialPosition();
+        int numberOfZeroTraversals = currentDialContext.getNumberOfZeroTraversals();
 
         if (numberOfClicks > 100) {
             int difference = numberOfClicks - 100;
+            numberOfZeroTraversals++;
             if (difference > 100) {
                 while (difference > 100) {
                     difference = difference - 100;
+                    numberOfZeroTraversals++;
                 }
             }
 
@@ -26,22 +28,29 @@ public class SafeDoor {
         }
 
         if (direction == DialDirection.LEFT) {
-            newDialPosition = currentDialPosition - numberOfClicks;
+            newDialPosition = currentDialContext.getCurrentDialPosition() - numberOfClicks;
             if (newDialPosition < 0) {
                 // plussing a minus
                 newDialPosition = 100 + newDialPosition;
+                if (currentDialContext.getCurrentDialPosition() != 0) {
+                    numberOfZeroTraversals++;
+                }
             }
-        }  else if (direction == DialDirection.RIGHT) {
-            newDialPosition = currentDialPosition + numberOfClicks;
-            if (newDialPosition >= 100) {
+        } else if (direction == DialDirection.RIGHT) {
+            newDialPosition = currentDialContext.getCurrentDialPosition() + numberOfClicks;
+            if (newDialPosition > 100) {
                 newDialPosition = newDialPosition - 100;
+                numberOfZeroTraversals++;
+            } else if (newDialPosition == 100) {
+                newDialPosition = 0;
+                // no zero traversals
             }
         }
         else {
             System.out.println("Invalid dial direction");
         }
 
-        return newDialPosition;
+        return DialContext.builder().currentDialPosition(newDialPosition).numberOfZeroTraversals(numberOfZeroTraversals).build();
     }
 
 }
--- src/test/java/org/paulholiday/DayOneTest.java
+++ src/test/java/org/paulholiday/DayOneTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DayOneTest {
 
@@ -23,38 +24,44 @@ public class DayOneTest {
     public void testSafeDoorDialMovements() {
 
         int currentDialPosition = 50;
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "L68");
-//        System.out.println("Current dialPosition: " + currentDialPosition);
-//        currentDialPosition = safeDoor.turnDial(50, "L168");
-//        System.out.println("Current dialPosition: " + currentDialPosition);
-//        currentDialPosition = safeDoor.turnDial(50, "L268");
-//        System.out.println("Current dialPosition: " + currentDialPosition);
+        DialContext dialContext = DialContext.builder().currentDialPosition(currentDialPosition).numberOfZeroTraversals(0).build();
+//        dialContext = safeDoor.turnDial(dialContext, "R1000");
+        dialContext = safeDoor.turnDial(dialContext, "L68");
+//        System.out.println("Current dialPosition: " + dialContext.getCurrentDialPosition());
+//        dialContext = safeDoor.turnDial(dialContext, "L168");
+//        System.out.println("Current dialPosition: " + dialContext.getCurrentDialPosition());
+//        dialContext = safeDoor.turnDial(dialContext, "L268");
+//        System.out.println("Current dialPosition: " + dialContext.getCurrentDialPosition());
 
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "L30");
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "R48");
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "L5");
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "R60");
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "L55");
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "L1");
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "L99");
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "R14");
-        currentDialPosition = safeDoor.turnDial(currentDialPosition, "L82");
-        System.out.println("Current dialPosition: " + currentDialPosition);
-        assertEquals(32, currentDialPosition);
+
+        dialContext = safeDoor.turnDial(dialContext, "L30");
+        dialContext = safeDoor.turnDial(dialContext, "R48");
+        dialContext = safeDoor.turnDial(dialContext, "L5");
+        dialContext = safeDoor.turnDial(dialContext, "R60");
+        dialContext = safeDoor.turnDial(dialContext, "L55");
+        dialContext = safeDoor.turnDial(dialContext, "L1");
+        dialContext = safeDoor.turnDial(dialContext, "L99");
+        dialContext = safeDoor.turnDial(dialContext, "R14");
+        dialContext = safeDoor.turnDial(dialContext, "L82");
+
+        assertEquals(32, dialContext.getCurrentDialPosition());
     }
 
     @Test
     public void dayOneTest() {
         int currentDialPosition = 50;
         int zeroPositionCount = 0;
+        DialContext dialContext = DialContext.builder().currentDialPosition(currentDialPosition).numberOfZeroTraversals(0).build();
         for (String dialMovement : fileContents) {
-            currentDialPosition = safeDoor.turnDial(currentDialPosition, dialMovement);
-            if (currentDialPosition == 0) {
+            dialContext = safeDoor.turnDial(dialContext, dialMovement);
+            if (dialContext.getCurrentDialPosition() == 0) {
                 zeroPositionCount++;
             }
         }
 
         System.out.println("zeroPositionCount: " + zeroPositionCount);
-
+        System.out.println("numberOfZeroTraversals: " + dialContext.getNumberOfZeroTraversals());
+        assertEquals(1150, zeroPositionCount);
+        assertEquals(6738, dialContext.getNumberOfZeroTraversals() + zeroPositionCount);
     }
 }
