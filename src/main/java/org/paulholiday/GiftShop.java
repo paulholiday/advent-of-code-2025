5e6f8c11053c3567a9300f6215bac051ffe2557d
paulholiday <paul.holiday@ensono.com>
2026-01-09 10:24:12 +0000

    Day Two part one plus start of day two

---
+++ src/main/java/org/paulholiday/GiftShop.java
@@ -0,0 +1,95 @@
+package org.paulholiday;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GiftShop {
+
+    public long getInvalidIDsTotal(String idRange) {
+
+        String[] ids = idRange.split("-");
+        String lastId = ids[1];
+
+        long sumOfInvalidIDs = 0;
+
+        long currentId = Long.parseLong(ids[0]);
+        long finalId = Long.parseLong(lastId);
+
+        String idToValidate;
+
+        while (currentId <= finalId) {
+            idToValidate = Long.toString(currentId);
+
+            boolean foundInvalidID = false;
+
+            if (idToValidate.length() % 2 == 0 && (matchingSections(idToValidate, 2) || allDigitsMatch(idToValidate))) {
+                sumOfInvalidIDs += currentId;
+                foundInvalidID = true;
+            }
+
+//            //divide by 5 then check matching fifths
+//            if (!foundInvalidID && idToValidate.length() % 5 == 0 && matchingFifths(idToValidate)) {
+//                sumOfInvalidIDs += currentId;
+//                foundInvalidID = true;
+//            }
+//
+//            // divide by 3 then check matching thirds
+//            if (!foundInvalidID && idToValidate.length() % 3 == 0) {
+//                sumOfInvalidIDs += currentId;
+//            }
+
+            currentId++;
+        }
+
+        return sumOfInvalidIDs;
+    }
+
+    private boolean allDigitsMatch(String id) {
+
+        boolean digitsMatch = false;
+        char[] chars = id.toCharArray();
+        for (int i = 0; i < id.length() - 1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                digitsMatch = true;
+            }
+            else {
+                digitsMatch = false;
+                break;
+            }
+        }
+
+        return digitsMatch;
+    }
+
+    private boolean matchingHalves(String id) {
+
+        String firstHalf = id.substring(0, id.length()/2);
+        String secondHalf = id.substring(id.length()/2);
+
+        return firstHalf.equals(secondHalf);
+    }
+
+    private boolean matchingSections(String id, int numberOfSections) {
+        int sectionLength = id.length()/numberOfSections;
+
+        Set<String> sections = new HashSet<>();
+
+        for (int i = 0; i < numberOfSections * sectionLength; i+=sectionLength) {
+            sections.add(id.substring(i, i + sectionLength));
+        }
+
+        return sections.size() == 1;
+    }
+
+    private boolean matchingFifths(String id) {
+        int fifthLength = id.length()/5;
+
+        Set<String> fifths = new HashSet<>();
+
+        for (int i = 0; i < fifthLength; i++) {
+            fifths.add(id.substring(i * 5, (i + 1) * 5));
+        }
+
+        return fifths.size() == 1;
+    }
+}
---
+++ src/test/java/org/paulholiday/DayTwoTest.java
@@ -0,0 +1,65 @@
+package org.paulholiday;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DayTwoTest {
+
+    private GiftShop giftShop;
+    private String[] idsArray;
+
+    @BeforeEach
+    void setup() {
+        Utils utils = new Utils();
+        List<String> fileContents = utils.getFileContentsByLine("src/main/resources/DayTwoInput.txt");
+        idsArray = fileContents.get(0).split(",");
+        giftShop = new GiftShop();
+    }
+
+//    @Test
+//    void testInvalidIDs() {
+//        long invalidIDs = giftShop.getInvalidIDs("11-22");
+//        assertEquals(2, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("95-115");
+//        assertEquals(1, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("998-1012");
+//        assertEquals(1, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("1188511880-1188511890");
+//        assertEquals(1, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("222220-222224");
+//        assertEquals(1, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("1698522-1698528");
+//        assertEquals(0, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("446443-446449");
+//        assertEquals(1, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("38593856-38593862");
+//        assertEquals(1, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("565653-565659");
+//        assertEquals(0, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("824824821-824824827");
+//        assertEquals(0, invalidIDs);
+//        invalidIDs = giftShop.getInvalidIDs("2121212118-2121212124");
+//        assertEquals(0, invalidIDs);
+//    }
+
+    @Test
+    void dayTwoTest() {
+
+        long invalidIDsSum = 0;
+
+        System.out.println("Number of id ranges: " + idsArray.length);
+
+        for (String id : idsArray) {
+            invalidIDsSum += giftShop.getInvalidIDsTotal(id);
+        }
+
+        System.out.println("DayTwoTest: invalidIDs: " + invalidIDsSum);
+        assertEquals(30323879646L, invalidIDsSum);
+
+    }
+}
